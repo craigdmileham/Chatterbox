3f025d70b59d36ac640ca678585c112bf846f561
craigdmileham <cmileham@drew.edu>
2016-04-25 03:01:40 -0400

    Now 2-way chat
    Added Server response functionality

--- Client.py
+++ Client.py
@@ -1,17 +1,30 @@
+#Client STREAM socket
 import socket
-
+#Initialize Client socket
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_address = (socket.gethostbyname('cmileham'), 9000)
+port = 9000
+server_address = (socket.gethostbyname('cmileham'), port)
 
 print("Connecting...")
-
+#connect to server
 client_sock.connect(server_address)
 
 print("Connected to server")
 
 while True:
+    #Get message to send to Server
     message = input("Type message to send: ")
     message = bytes(message, encoding='UTF-8')
+    #Send message to Server
     client_sock.sendall(message)
-    #recvd_message = client_sock.recv(4096)
+    #Receive response from Server
+    recvd_message = client_sock.recv(4096)
+    if not recvd_message:
+        #If no message received, close connection
+        print("Disconnected from Server")
+        client_sock.close()
+        break
+    else:
+        #If message received, print message
+        recvd_message = recvd_message.decode(encoding = 'UTF-8')
+        print("Server said: " + recvd_message)
--- Server.py
+++ Server.py
@@ -1,21 +1,34 @@
 # Server STREAM socket
 import socket
-
+#Initialize server socket
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 9000
-
+#Bind socket to Host and Port
 serv_sock.bind((socket.gethostbyname('cmileham'), port))
-
-serv_sock.listen(2)
+#Set socket to listen for connections
+serv_sock.listen(1)
 
 print("Server Started on port " + str(port))
 
-print("Waiting for a connection")
-conn, client_address = serv_sock.accept()
-print("Connected to " + str(client_address))
-
 while True:
+    print("Waiting for a connection")
+    #Accept connection to Client
+    conn, client_address = serv_sock.accept()
+    print("Connected to " + str(client_address))
 
-    recvd_message = conn.recv(4096)
-    recvd_message = recvd_message.decode(encoding = 'UTF-8')
-    print(str(client_address) + " said: " + recvd_message)
+    while True:
+        #Receive message from Client
+        recvd_message = conn.recv(4096)
+        #If no message is received, close connection
+        if not recvd_message:
+            print("Client has Disconnected")
+            conn.close()
+            break
+        else:
+        # Print message
+            recvd_message = recvd_message.decode(encoding = 'UTF-8')
+            print(str(client_address) + " said: " + recvd_message)
+        #Get response from Server to Client
+            rply_message = input("Type message to send: ")
+            rply_message = bytes(rply_message, encoding = 'UTF-8')
+            conn.sendall(rply_message)
